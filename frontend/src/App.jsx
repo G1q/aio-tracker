6c2e851699703e8d49ed763a84c9aecf8b83bae6
G1q <beresteanu.geroge@gmail.com>
2024-05-24 12:01:03 +0300

    MUI tests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Measurements from './pages/measurements/Measurements';
 import Water from './pages/water/Water';
+import Login from './pages/auth/Login';
+import SimpleHeaderLayout from './layouts/SimpleHeaderLayout';
 
 function App() {
 	return (
@@ -21,6 +23,12 @@ function App() {
 				path="/water"
 				element={<Water />}
 			/>
+
+			{/* Simple header pages */}
+			<Route path='/' element={<SimpleHeaderLayout />}>
+				<Route path='/login' element={<Login />} />
+			</Route>
+
 			<Route
 				path="*"
 				element={<h1>Page not found</h1>}
